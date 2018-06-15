5fc752a3563363f2a60e295baa64701f58b4c088
Amy Groshek <amygroshek@gmail.com>
2018-06-15 11:28:57 -0500

    Switch render text to conditional

--- layout/frontpage.php
+++ layout/frontpage.php
@@ -58,7 +58,7 @@ if (isloggedin()) {
     if (!empty($PAGE->theme->settings->avail_courses_welcome)) {
         echo $user;
         $welcome_header = 'Welcome'.' '.$user['firstname'];
-        $availcoursewelcome = html_writer::tag('h2', $welcome_header, array(class => 'front-avail-welcome use1st-welcome'));
+        $availcoursewelcome = html_writer::tag('h2', $welcome_header, array('class' => 'front-avail-welcome use1st-welcome'));
         $availcoursewelcome .= theme_user1st_get_setting('avail_courses_welcome', true);
     }
 
